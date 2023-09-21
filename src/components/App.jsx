70a8720728d81c664587b6fe75cb42b530be2aac
wangyucen <43635632+wangyucen@users.noreply.github.com>
2023-09-21 17:29:33 +0200

    Code works to finish up the assignment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,47 @@
 import "./App.css";
 import Layout from "./layout/Layout";
+import Header from "./layout/Header";
+import { UserProvider } from "../client/context/UserContext";
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import ErrorBoundary from "./base/ErrorBoundary";
+import Reviews from "./pages/Reviews";
+import Sales from "./pages/Sales";
 
 function App() {
   return (
     <>
-      <Layout></Layout>
+      <UserProvider>
+        <Layout>
+          <Header />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ErrorBoundary>
+                  <Home />
+                </ErrorBoundary>
+              }
+            />
+            <Route
+              path="/reviews"
+              element={
+                <ErrorBoundary>
+                  <Reviews />
+                </ErrorBoundary>
+              }
+            />
+            <Route
+              path="/sales"
+              element={
+                <ErrorBoundary>
+                  <Sales />
+                </ErrorBoundary>
+              }
+            />
+          </Routes>
+        </Layout>
+      </UserProvider>
     </>
   );
 }
